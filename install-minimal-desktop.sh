127d2e8953193604d158201906009eaf78e01263
Cazzbr <31737100+Cazzbr@users.noreply.github.com>
2025-08-11 10:11:36 -0300

    Update install-minimal-desktop.sh

--- install-minimal-desktop.sh
+++ install-minimal-desktop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Ensure script is run as root
-if [[ $EUID -ne 0 ]]; then
-  echo "Please run as root."
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run as root." >&2
   exit 1
 fi
 
